414664475d5ef411ea84c7d0760e4607a6a65adf
krishna kumar kushwaha <krishnakushwaha743@gmail.com>
2023-08-07 19:41:42 +0530

    bugfix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import MoviesTable from './components/MoviesTable';
-import WelcomePage from './pages/welcome-page/WelcomePage';
 
 
 function App() {
   return (
     <div className="App">
       <MoviesTable />
-      <WelcomePage/>
     </div>
   );
 }
